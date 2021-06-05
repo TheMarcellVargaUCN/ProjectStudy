30f641217223aec84a6481ebdfd54c1002e5cea3
DominikRostek <1089269@ucn.dk>
2021-06-05 17:27:05 +0200

    Update Javascript.js

--- assets/js/Javascript.js
+++ assets/js/Javascript.js
@@ -6,3 +6,28 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+window.onscroll = function(){onScrollChecks()};
+let plane = document.getElementById('plane');
+let car = document.getElementById('car');
+
+function onScrollChecks(){
+	//console.log(window.pageYOffset);
+    if(window.pageYOffset < 1000){
+       car.style.display = "block";
+   }
+	if(window.pageYOffset >= 1000){
+        car.style.display = "none";
+	}
+    if(window.pageYOffset > 1000){
+       plane.style.display = "block";
+   }
+	if(window.pageYOffset <= 1000){
+        plane.style.display = "none";
+	}
+
+    if(window.pageYOffset > 2630){
+       plane.style.display = "none";
+   }
+
+ }
